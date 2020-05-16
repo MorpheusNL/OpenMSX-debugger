8dc0674f3319169bb1b4e8738ee93a9003ef7598
Chris Smit <smit.chris73@gmail.com>
2020-05-16 11:34:25 +0200

    Initial Commit

---
+++ MVCtest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MVCtest
+//
+//  Created by Chris Smit on 16/05/2020.
+//  Copyright © 2020 Chris Smit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MVCtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MVCtest
+//
+//  Created by Chris Smit on 16/05/2020.
+//  Copyright © 2020 Chris Smit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
