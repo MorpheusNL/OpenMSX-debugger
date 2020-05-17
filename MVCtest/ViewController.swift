fbc638bbbb9fd6b6d08a3f3d64e330ecfb5b5c87
Chris Smit <smit.chris73@gmail.com>
2020-05-17 16:35:54 +0200

    Initial commit. Application has been tested to function.

--- MVCtest/ViewController.swift
+++ MVCtest/ViewController.swift
@@ -9,11 +9,14 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var contentsLabel: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // setup Observer to receive a message when the file has downloaded
+        NotificationCenter.default.addObserver(self, selector:#selector(onDidReceiveMessage), name: .didReceiveData, object: nil)
     }
 
     override var representedObject: Any? {
@@ -22,6 +25,37 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func onConnectButtonPressed(_ sender: NSButton) {
+//        let myFileDialog = NSOpenPanel()
+//        myFileDialog.canChooseFiles = true
+//        myFileDialog.canChooseDirectories = true
+//        myFileDialog.resolvesAliases = true
+//        myFileDialog.allowsMultipleSelection = true
+//        myFileDialog.isAccessoryViewDisclosed = true
+//        myFileDialog.runModal()
+//        print(myFileDialog.url?.path)
+        sender.title = "Reset"
+        contentsLabel.stringValue = ""
+    }
+    
+    @IBAction func onLoadButtonPressed(_ sender: Any) {
+        let MySock = SockData()
+//        let message = MySock.loadData()
+//        contentsLabel.stringValue = message
+        MySock.loadData()
+        
+    }
+    
+    @objc func onDidReceiveMessage(_ notification: Notification) {
+        if let data = notification.userInfo as? [String: String] {
+            if let message = data["MESSAGE"] {
+                contentsLabel.stringValue = message
+            }
+        }
+    }
+    
 }
 
+extension Notification.Name {
+    static let didReceiveData = Notification.Name("didReceiveData")
+}
