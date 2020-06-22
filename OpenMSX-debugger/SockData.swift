a69ad46e37f623a3a4510fa87e46d2a2a53f8240
Chris Smit <smit.chris73@gmail.com>
2020-06-22 19:31:38 +0200

    Implemented socket and added GUI features. GUI constraints still not set (to be updated).

---
+++ OpenMSX-debugger/SockData.swift
@@ -0,0 +1,188 @@
+//
+//  SockData.swift
+//  MVCtest
+//
+//  Created by Chris Smit on 16/05/2020.
+//  Copyright © 2020 Chris Smit. All rights reserved.
+//
+
+import Foundation
+
+/* SockData Class
+    func loadData()
+    Reads data from a file or named pipe in the $TMPDIR location. Application Sandbox has been disabled in XCode to
+    allow access to files outside the Sandbox.
+    Reading occurs blocking and when reading a named pipe the application stalls until data is fed to the pipe.
+    Making a pipe in a terminal: mkfifo namedpipe
+    Writing data to a named pipe: cat Readme.txt > pipe
+    Note a pipe can be recognized by the p before the access rights, e.g.:
+    prw-r--r--     1 chris  staff       0 17 mei 16:17 textpipe
+ */
+struct SockData {
+    let socketDescriptor : Int32
+    let receiveBufferSize : Int = 512
+    
+    // create dispatch queue for asynchronous socket operation
+//    let dqueue = DispatchQueue(label: "MorpheusNL")
+    
+    init() {
+        socketDescriptor = socket(AF_UNIX, SOCK_STREAM, 0)
+        if socketDescriptor < 0 {
+            print("Failed to initialize socket: \(errno)")
+        } else {
+            print("Socket created")
+        }
+        
+//        connectSocket(pathToSocket: "/var/folders/m1/7g9ngbgj33b5qq_s8b4wv3dr0000gn/T//openmsx-chris/socket.20178")
+    }
+    
+    // load data from a file
+    func loadData() {
+        
+        var content = "Could not Read the File"
+        
+        let tmpPath = FileManager.default.temporaryDirectory
+        
+//        print("Temporary directory:\n\(tmpPath.path)")
+
+        let filePath = tmpPath
+            .appendingPathComponent("nl.CSolutions.MVCtest/Readme.txt")
+              
+        print("Reading file or named pipe: \(filePath.path)")
+        
+        if let fh = FileHandle.init(forReadingAtPath: filePath.path) {
+            let fileContent = fh.readDataToEndOfFile()
+            let str = String(decoding: fileContent, as: UTF8.self)
+            content = str
+//            print("Read file:\n \(str)")
+            fh.closeFile()
+        }
+        
+        let message = ["MESSAGE": content ]
+        NotificationCenter.default.post(name: .didReceiveData, object: nil, userInfo: message)
+    }
+    
+    func connectSocket(pid:Int) {
+        let socketFileName = "socket.\(pid)"
+        let userName = NSUserName()
+        let socketName = "openmsx-\(userName)"
+        
+        let socketPath = FileManager.default.temporaryDirectory
+        .appendingPathComponent(socketName)
+        .appendingPathComponent(socketFileName)
+        print(socketPath.path)
+        connectSocket(pathToSocket: socketPath.path)
+        let message = ["STATUS": "CONNECTED"]
+        NotificationCenter.default.post(name: .didReceiveData, object: nil, userInfo: message)
+    }
+    
+    // connectSocket creates sockaddr_un structure using pathToSocket and connects to socket
+    func connectSocket(pathToSocket: String) {
+        var sun_path = (Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0), Int8(0),
+            Int8(0), Int8(0), Int8(0), Int8(0) )
+        withUnsafeMutableBytes(of: &sun_path, {
+            ptr in ptr.copyBytes(from: pathToSocket.utf8.prefix(pathToSocket.count))
+        })
+        var serverAddress = sockaddr_un(sun_len: UInt8(pathToSocket.count), sun_family: sa_family_t(AF_UNIX), sun_path: sun_path)
+
+        let connectResult = withUnsafePointer(to: &serverAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1, {connect(socketDescriptor, $0, socklen_t(MemoryLayout<sockaddr_un>.stride))})
+        })
+        if connectResult < 0 {print("Error connecting to socket: \(errno)")}
+        else {
+            print("Connected")
+        }
+        readSocket()
+    }
+    
+    // read data from already connected socket
+    func readSocket() {
+        let receiveBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: receiveBufferSize)
+        
+        receiveBuffer.initialize(repeating: 0, count: receiveBufferSize)
+        
+//        defer {
+//            receiveBuffer.deinitialize(count: receiveBufferSize)
+//            receiveBuffer.deallocate()
+//        }
+        let receiveRawBuffer = UnsafeMutableRawPointer(receiveBuffer)
+
+        // TODO: fix the queue
+        DispatchQueue.main.async {
+            _ = recv(self.socketDescriptor, receiveRawBuffer, self.receiveBufferSize, 0)
+            let rxRawBufferPointer = UnsafeRawBufferPointer(start: receiveRawBuffer, count: self.receiveBufferSize)
+            let content = self.convertSpecialCharacters(string: String.init(bytes: rxRawBufferPointer, encoding: .utf8) ?? "No reply")
+            let str = content.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+            let message = ["MESSAGE":  str, "RAW": content]
+            NotificationCenter.default.post(name: .didReceiveData, object: nil, userInfo: message)
+        }
+                
+
+    }
+    
+    // read data from already connected socket
+    func readSocket(name aName: NSNotification.Name) {
+            let receiveBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: receiveBufferSize)
+            
+            receiveBuffer.initialize(repeating: 0, count: receiveBufferSize)
+            
+    //        defer {
+    //            receiveBuffer.deinitialize(count: receiveBufferSize)
+    //            receiveBuffer.deallocate()
+    //        }
+            let receiveRawBuffer = UnsafeMutableRawPointer(receiveBuffer)
+
+            // TODO: fix the queue
+            DispatchQueue.main.async {
+                _ = recv(self.socketDescriptor, receiveRawBuffer, self.receiveBufferSize, 0)
+                let rxRawBufferPointer = UnsafeRawBufferPointer(start: receiveRawBuffer, count: self.receiveBufferSize)
+                let content = self.convertSpecialCharacters(string: String.init(bytes: rxRawBufferPointer, encoding: .utf8) ?? "No reply")
+                let str = content.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+                let message = ["BREAKPOINTS":  str ]
+                NotificationCenter.default.post(name: aName, object: nil, userInfo: message)
+            }
+                    
+
+        }
+    
+    func convertSpecialCharacters(string: String) -> String {
+            var newString = string
+            let char_dictionary = [
+                "&amp;" : "&",
+                "&lt;" : "<",
+                "&gt;" : ">",
+                "&quot;" : "\"",
+                "&apos;" : "'"
+            ];
+            for (escaped_char, unescaped_char) in char_dictionary {
+                newString = newString.replacingOccurrences(of: escaped_char, with: unescaped_char, options: NSString.CompareOptions.literal, range: nil)
+            }
+            return newString
+    }
+
+    func writeSocket(message: String) {
+        if let data = message.data(using: .utf8) {
+            data.withUnsafeBytes( {write(socketDescriptor, $0, message.count)} )
+        }
+        readSocket()
+        
+    }
+    
+    func setBreakPoint(message: String) {
+        if let data = message.data(using: .utf8) {
+            data.withUnsafeBytes( {write(socketDescriptor, $0, message.count)} )
+        }
+        readSocket(name: .didReceiveBreakpoint)
+        
+    }
+}
+
---
+++ OpenMSX-debugger/ViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  MVCtest
+//
+//  Created by Chris Smit on 16/05/2020.
+//  Copyright © 2020 Chris Smit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var contentsLabel: NSTextField!
+    @IBOutlet weak var pidNumber: NSTextField!
+    
+    @IBOutlet weak var assemblyFilePath: NSTextField!
+    @IBOutlet weak var assemblyMemoryLocation: NSTextField!
+    
+    @IBOutlet weak var connectedIndicatorLabel: NSTextField!
+
+    @IBOutlet weak var logText: NSTextFieldCell!
+    
+    
+    let MySock = SockData()
+    
+    var selectedFile : URL?
+    
+    // create empty breakpoint dictionary
+    var breakpoints = [1: "", 2: "", 3: "", 4: ""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup Observer to receive a message when the file has downloaded
+        NotificationCenter.default.addObserver(self, selector:#selector(onDidReceiveMessage), name: .didReceiveData, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(onDidReceiveBreakpoint), name: .didReceiveBreakpoint, object: nil)
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func pidEntered(_ sender: Any) {
+        if let pid = Int(pidNumber.stringValue) {
+            MySock.connectSocket(pid: pid)
+        } else {
+            let alert = NSAlert()
+            alert.messageText = "PID not valid"
+            alert.informativeText = "Please enter a valid PID number for the OpenMSX emulator. To find this number open a terminal and type:\n\nlsof -U | grep -i openmsx\n\nThe PID is the number of the extension of the socket filename."
+            alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
+        }
+    }
+    
+    @IBAction func onRegsButtonPressed(_ sender: NSButton) {
+        MySock.writeSocket(message: "<command>cpuregs</command>")
+    }
+    
+    @IBAction func vdpregsButtonPressed(_ sender: Any) {
+        MySock.writeSocket(message: "<command>vdpregs</command>")
+    }
+    
+    @IBAction func onLoadButtonPressed(_ sender: Any) {
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = true
+        panel.beginSheetModal(for: view.window!) { (result) in
+            if result.rawValue == NSFileHandlingPanelOKButton {
+                self.selectedFile = panel.urls[0]
+                self.assemblyFilePath.stringValue = self.selectedFile?.path ?? ""
+                self.assemblyFilePath.isHidden = false
+                let filePath = self.selectedFile?.path
+                let memLocation = self.assemblyMemoryLocation.stringValue
+                let str = "<command>load_debuggable memory \(filePath ?? "") 0x\(memLocation)</command>"
+                self.MySock.writeSocket(message: str)
+            }
+        }
+    }
+    
+    @IBAction func togglePowerSelected(_ sender: NSButtonCell) {
+        switch sender.state {
+        case .on:
+            MySock.writeSocket(message: "<command>set power on</command>")
+        case .off:
+            MySock.writeSocket(message: "<command>set power off</command>")
+        default:
+            MySock.writeSocket(message: "<command>set power on</command>")
+        }
+    }
+    
+    @IBAction func infoButtonPressed(_ sender: Any) {
+        let alert = NSAlert()
+        alert.messageText = "OpenMSX Debugger"
+        alert.informativeText = "Connect the OpenMSX Debugger with OpenMSX running. First determine the PID of the OpenMSX emulator and click Connect. The Z80 registers can be read out using Regs. If needed power on/off your virtual MSX by clicking the power button."
+        alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
+    }
+    
+    @IBAction func debugStepButtonPressed(_ sender: Any) {
+        MySock.writeSocket(message: "<command>debug step</command>")
+        
+    }
+    
+    @IBAction func debugContinueButtonPressed(_ sender: Any) {
+        MySock.writeSocket(message: "<command>debug cont</command>")
+    }
+    
+    @IBOutlet weak var bpAddress: NSTextField!
+   
+    @IBOutlet weak var breakpointPopUpButton: NSPopUpButtonCell!
+    
+    @IBAction func setBreakPointButtonPressed(_ sender: Any) {
+        let bpMemLocation = bpAddress.stringValue
+        // TODO: check if connected
+        if bpMemLocation != "" {
+            MySock.writeSocket(message: "<command>debug set_bp \(bpMemLocation)</command>")
+        }
+    }
+    
+    @IBAction func refreshBreakpointButtonPressed(_ sender: Any) {
+        MySock.setBreakPoint(message: "<command>debug list_bp</command>")
+    }
+    
+    @IBAction func deleteBreakPointButtonPressed(_ sender: Any) {
+        if let bpString = breakpointPopUpButton.selectedItem?.title {
+            let breakPointID = bpString.split(separator: " ", maxSplits: 1, omittingEmptySubsequences: true)
+            print(breakPointID[0])
+            breakpointPopUpButton.removeItem(withTitle: bpString)
+            MySock.writeSocket(message: "<command>debug remove_bp \(breakPointID[0])</command>")
+        }
+        
+    }
+    
+    @objc func onDidReceiveMessage(_ notification: Notification) {
+        if let data = notification.userInfo as? [String: String] {
+            if let message = data["MESSAGE"] {
+                contentsLabel.stringValue = message
+            }
+            if let message = data["STATUS"] {
+                if message == "CONNECTED" {
+                    self.connectedIndicatorLabel.isHidden = false
+                }
+            }
+            if let message = data["RAW"] {
+                self.logText.stringValue = message
+            }
+        }
+    }
+    
+    @objc func onDidReceiveBreakpoint(_ notification: Notification) {
+        if let data = notification.userInfo as? [String: String] {
+            if let message = data["BREAKPOINTS"] {
+                breakpointPopUpButton.removeAllItems()
+                for line in message.split(separator: "\n", maxSplits: 99, omittingEmptySubsequences: true) {
+                    breakpointPopUpButton.addItem(withTitle: String(line))
+                }
+                
+            }
+        }
+    }
+}
+
+extension Notification.Name {
+    static let didReceiveData = Notification.Name("didReceiveData")
+    static let didReceiveBreakpoint = Notification.Name("didReceiveBreakpoint")
+}
