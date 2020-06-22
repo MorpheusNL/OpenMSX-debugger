1f0f42ae6503d02ddb16a35422a8a6772427923d
Chris Smit <smit.chris73@gmail.com>
2020-06-22 19:51:58 +0200

    test

---
+++ OpenMSX-debugger/HelpWindow.swift
@@ -0,0 +1,23 @@
+//
+//  HelpWindow.swift
+//  MVCtest
+//
+//  Created by Chris Smit on 01/06/2020.
+//  Copyright © 2020 Chris Smit. All rights reserved.
+//
+
+import Cocoa
+
+class HelpWindow: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func enteredHelpQuery(_ sender: Any) {
+        
+    }
+    
+    
+}
